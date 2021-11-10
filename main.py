2bc18b2f832bd6f99050a257ce4b25739985e244
Shane McDonough <shanemcdman@gmail.com>
2021-11-09 23:17:18 -0800

    Initial

---
+++ main.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+from sorts import bubble_sort, merge_sort, selection_sort, radix_sort, radix_sort_in_place
+from typing import List, TypeVar, Callable
+from random import randint
+import time
+
+T = TypeVar('T') # allows for typehinting unknown types
+
+def rand_arr(size: int, min_val: int = 0, max_val: int = 10) -> List[int]:
+    '''
+    creates a list of random numbers
+    :size: size of return list
+    :min_val: minimum value for numbers in list
+    :max_val: maximum value for numbers in list
+    :returns: a list of random numbers
+    '''
+    return [randint(min_val, max_val) for i in range(size)]
+
+def is_sorted(arr: List[T]) -> bool:
+    '''
+    print array and check if sorted
+    :arr: the list of items
+    :returns: True if the list is sorted
+    '''
+    size = len(arr) # size of array
+    if size <= 1: # size is 0 or 1
+        return True # with 0 or 1 element there is no order
+    for i in range(size - 1): # iterate from 0 to size - 2
+        if arr[i + 1] < arr[i]: # if the second term is less than the first term
+            return False # the list isnt sorted
+    return True # otherwise the list is sorted
+
+
+def test_sort(sort_func: Callable[[List[int]], List[int]], number_of_items: int = 40, max_val:int = 100, print_lists: bool = True, print_time: bool = True):
+    '''
+    test to make sure a sort function works
+    :sort_func: the function that will return a sorted list
+    '''
+    print('-' * 10, f'testing {sort_func.__name__}', '-' * 10)
+    arr = rand_arr(number_of_items, max_val = max_val) # get random array
+    if print_lists:
+        print('before:', arr) # show list
+    start = time.time()
+    arr = sort_func(arr) # sort list
+    end = time.time()
+    if print_lists:
+        print('sorted:', arr) # show array
+    if print_time:
+        print('time elapsed:', end - start)
+    assert is_sorted(arr) # check list
+
+def main():
+    '''driver code'''
+    t = lambda s: test_sort(s, 10, 10 ** 3, True, True)
+    t(bubble_sort)
+    t(merge_sort)
+    t(selection_sort)
+    t(radix_sort)
+    t(radix_sort_in_place)
+
+if __name__ == '__main__':
+    main() # run the main method
---
+++ sorts.py
@@ -0,0 +1,114 @@
+from math import log, floor
+from typing import List, TypeVar
+
+T = TypeVar('T') # allows for typehinting unknown types
+
+def bubble_sort(arr: List[T]) -> List[T]:
+    '''
+    perform the bubble sort algorithm
+    :arr: the list of items
+    :returns: a sorted list of items
+    '''
+    result = arr[:] # shallow copy list
+    size = len(arr) # size of list
+    for i in range(size): # iterate over numbers 0 thru size - 1
+        for j in range(size - i - 1): # iterate over numbers thru size -i - 2
+            if result[j] > result[j + 1]: # if the first item is more than the second
+                result[j], result[j + 1] = result[j + 1], result[j] # swap them
+    return result
+
+def merge(a: List[T], b: List[T]) -> List[T]:
+    '''
+    merge two sorted lists
+    :a: the first list
+    :b: the second list
+    :returns: a list containing all of the elements of the two arguments sorted
+    '''
+    result = [] # create empty list
+    while len(a) > 0 and len(b) > 0:
+        if a[0] < b[0]: # if top item in first is less than top item in second
+            result.append(a.pop(0)) # remove from first list and add to result
+        else: # otherwise
+            result.append(b.pop(0)) # remove from second list and add to result
+    # at this point either a or b is empty
+    if len(a) == 0: # a is empty
+        while len(b) > 0: # while b is not empty
+            result.append(b.pop(0)) # remove from second list and add to result
+    else: # b is empty
+        while len(a) > 0: # while a is not empty
+            result.append(a.pop(0)) # remove from first list and add to result
+    return result
+
+def merge_sort(arr: List[T]) -> List[T]:
+    '''
+    perform the merge sort algorithm
+    :arr: the list to sort
+    :returns: the sorted list
+    '''
+    size = len(arr) # size of list
+    if size <= 1: # if there is one element in list or empty
+        return arr[:] # return a shallow copy of the given list
+    return merge( # merge following lists
+            merge_sort(arr[:size // 2]), # sort first half of list
+            merge_sort(arr[size // 2:]) # sort second half
+            )
+
+def selection_sort(arr: List[T]) -> List[T]:
+    '''
+    Sorts a list using selection sort
+    Type T must have < operator imlemented
+    :arr: the list to be sorted
+    :returns: a sorted list
+    '''
+    size = len(arr)
+    if size <= 1:
+        return arr[:] # return shallow copy of list
+    arr = arr[:] # shallow copy list
+    for i in range(0, size): # cycle over i from 0 to size - 1
+        smallest_idx = i # smallest size starts at first index
+        for j in range(i + 1, size): # cycle over j from i + 1 to size - 1
+            if arr[j] < arr[smallest_idx]: # if current val is smaller
+                smallest_idx = j # set current to smallest
+        arr[i], arr[smallest_idx] = arr[smallest_idx], arr[i] # swap smallest and first
+    return arr
+
+def radix_sort(arr: List[int], base: int = 2) -> List[int]:
+    '''
+    Sorts a list using radix sort
+    :arr: the list to be sorted
+    :returns: a sorted list
+    '''
+    size = len(arr) # size of array
+    result = arr[:] # shallow copy array
+    buckets = [[] for _ in range(base)] # create empty buckets
+    highest_power = max(map(lambda x: floor(log(x, base)) if x != 0 else 1, arr)) + 1 # find highest power of base
+    for i in range(0, highest_power): # cycle from 0 to highest power
+        divisor = base ** i # the power of the current base
+        mod = base ** (i + 1) # one more than the power of the current base
+        for item in result:
+            buckets[item % mod // divisor].append(item) # group into buckets
+        result.clear() # clear result
+        for bucket in buckets: # pull out of buckets
+            while len(bucket) > 0:
+                result.append(bucket.pop(0)) # put into result
+    return result
+
+def radix_sort_in_place(arr: List[int], base: int = 10) -> List[int]:
+    '''
+    Sorts a list using radix sort in place
+    :arr: the list to be sorted
+    :returns: a sorted list
+    '''
+    size = len(arr) # size of array
+    result = arr[:] # shallow copy array
+    highest_power = max(map(lambda x: floor(log(x, base)) if x != 0 else 1, arr)) + 1 # find highest power of base
+    for i in range(0, highest_power): # cycle from 0 to highest power
+        divisor = base ** i # the power of the current base
+        mod = base ** (i + 1) # one more than the power of the current base
+        for j in range(size):
+            for k in range(j, 0, -1):
+                if (result[k - 1] % mod // divisor) > (result[k] % mod // divisor):
+                    result[k], result[k - 1] = result[k -1], result[k]
+                else:
+                    break
+    return result
